67a93267c252c8aee103e90179d29a2e59ae4979
unnimayacu <unnimaya@reontel.com>
2022-05-16 17:55:25 +0530

    search

--- src/Maincomponent/Search.js
+++ src/Maincomponent/Search.js
@@ -7,7 +7,9 @@ function Search() {
   return (
     <>
       <div className="row py-4 p-4 search__text ">
-          
+          <div className="search__">
+
+          </div>
         <input
           type={"text" } 
           className={`form-control  search__text p-2 shadow`}
---
+++ src/Maincomponent/Studentrecord.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Maincards from "./Maincards";
+function Studentrecord() {
+  return (
+    <>
+     <div>
+         <Maincards/>
+     </div>
+
+    </>
+  )
+}
+
+export default Studentrecord
